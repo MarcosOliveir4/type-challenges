aebbeb1ec72962da688d86fdfaf3a3dda461dedf
MarcosOliveir4 <marcosassis071@gmail.com>
2022-07-22 12:26:58 -0300

    adding the answer to question 189

--- questions/00189-easy-awaited/template.ts
+++ questions/00189-easy-awaited/template.ts
@@ -1,1 +1,7 @@
-type MyAwaited = any
+// type MyAwaited<T> = T extends Promise<infer P> ? MyAwaited<P> : T
+
+type MyAwaited<T extends Promise<any>> = T extends Promise<infer K>
+  ? K extends Promise<any>
+    ? MyAwaited<K>
+    : K
+  : T
