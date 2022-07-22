09dde1cd16feb2faaa0cf70bf7dad4dd7bef8b08
MarcosOliveir4 <marcosassis071@gmail.com>
2022-07-22 11:33:15 -0300

    adding the answer to question 18

--- questions/00018-easy-tuple-length/template.ts
+++ questions/00018-easy-tuple-length/template.ts
@@ -1,1 +1,3 @@
-type Length<T> = any
+// type Length<T extends readonly any[]> = T['length']
+// type Length<T extends readonly unknown[]> = T['length']
+type Length<T extends readonly string[]> = T['length']
